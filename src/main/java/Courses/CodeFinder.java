9d8756bb4045d8194ffde8939789a5bf7405d409
Ethan Patterson <pattee14@mcmaster.ca>
2023-09-24 02:09:05 -0400

    works for grad courses now

--- src/main/java/Courses/CodeFinder.java
+++ src/main/java/Courses/CodeFinder.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CodeFinder {
-    private final Pattern undergradCoursePattern = Pattern.compile("[A-Z]{3,} [0-9][A-Z0-9]{2}[0-9][A|B]*");
+    private final Pattern coursePattern = Pattern.compile("[[A-Z]{3,} ]+[0-9][A-Z0-9]{2}[0-9][A|B]*|[[A-Z]{2,} ]+[A-Z]*[0-9]{3,}[A|B]*");
 
     private Matcher matcher;
     private final String line;
@@ -13,7 +13,7 @@ public class CodeFinder {
     }
 
     public boolean containsCourse() {
-        matcher = undergradCoursePattern.matcher(line);
+        matcher = coursePattern.matcher(line);
         return matcher.find();
     }
 
