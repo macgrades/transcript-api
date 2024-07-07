e2a6aab5addf8e7d659debecbd4b26421fa902b7
Ethan Patterson <pattee14@mcmaster.ca>
2024-07-07 16:24:48 -0400

    Add functions for tracking number of errors and page visits.

---
+++ src/main/java/com/function/ritual17/ErrorReportFunction.java
@@ -0,0 +1,29 @@
+package com.function.ritual17;
+
+import java.util.Optional;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+public class ErrorReportFunction {
+    
+    @FunctionName("log-error")
+    public HttpResponseMessage logError(
+            @HttpTrigger(
+                name = "req",
+                methods = {HttpMethod.GET, HttpMethod.POST},
+                authLevel = AuthorizationLevel.ANONYMOUS)
+                HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        context.getLogger().severe("This is a severe log message.");
+        return request.createResponseBuilder(HttpStatus.OK).build();
+    }
+}
---
+++ src/main/java/com/function/ritual17/PageVisitFunction.java
@@ -0,0 +1,27 @@
+package com.function.ritual17;
+
+import java.util.Optional;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+public class PageVisitFunction {
+    @FunctionName("visit")
+    public HttpResponseMessage pageVisit(
+            @HttpTrigger(
+                name = "req",
+                methods = {HttpMethod.GET, HttpMethod.POST},
+                authLevel = AuthorizationLevel.ANONYMOUS)
+                HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a VISIT request.");
+
+        return request.createResponseBuilder(HttpStatus.OK).build();
+    }
+}
